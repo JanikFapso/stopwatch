48195ddac8fefbbc3b5d7d13156c61cdeef3c380
JanikFapso <h.fapso@seznam.cz>
2025-03-06 20:20:33 +0100

    add the ability to stopwatch 2 people

--- main.py
+++ main.py
@@ -3,28 +3,55 @@ import time
 csv_file = open("data.csv", "w")
 
 def stopwatch():
-    times = ""
+    rosta_times = ""
+    janik_times = ""
 
     while True:
-        input1 = input("Press Enter to start the stopwatch")
-        if input1 == "q":
+        for i in range(2):
+            input1 = input("Press Enter to start the stopwatch")
+            if input1 == "q":
+                break
+            start_time = time.time()
+
+            input2 = input("Press Enter to stop the stopwatch")
+            end_time = time.time()
+            
+            elapsed_time = end_time - start_time
+            print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+            rosta_times = rosta_times + str(round(elapsed_time, 2)) + ", "
+       
+            if input2 == "q":
+                break
+
+        if input1 or input2 == "q": 
             break
-        start_time = time.time()
 
-        input2 = input("Press Enter to stop the stopwatch")
-        end_time = time.time()
-        
-        elapsed_time = end_time - start_time
-        print(f"Elapsed time: {elapsed_time:.2f} seconds")
+        for i in range(2):
+            input1 = input("Press Enter to start the stopwatch")
+            if input1 == "q":
+                break
+            start_time = time.time()
 
-        times = times + str(round(elapsed_time, 2)) + ", "
+            input2 = input("Press Enter to stop the stopwatch")
+            end_time = time.time()
+            
+            elapsed_time = end_time - start_time
+            print(f"Elapsed time: {elapsed_time:.2f} seconds")
 
-        if input2 == "q":
+            janik_times = janik_times + str(round(elapsed_time, 2)) + ", "
+
+            if input2 == "q":
+                break
+
+        if input1 or input2 == "q": 
             break
 
-    times = times[:-2]
-    print(times)
-    csv_file.write(times)
+    rosta_times = rosta_times[:-2]
+    janik_times = janik_times[:-2]
+    print(rosta_times)
+    print(janik_times)
+    csv_file.write(f"{rosta_times}\n{janik_times}")
     csv_file.close()
 
 stopwatch()
